66b592a6a8705ee66b5625b59bedb6f63e3ddb72
sandeepsalkuti <salkutisandeep222@gmail.com>
2021-08-09 21:05:50 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CustomerPageComponent} from '../app/customers/customer-page/customer-page.component';
+import {DeliveriesPageComponent} from '../app/deliveries/deliveries-page/deliveries-page.component';
+import {OrderPageComponent} from '../app/orders/order-page/order-page.component';
+import {ProductPageComponent} from '../app/products/product-page/product-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component:  },
+  { path: 'customer-page', component: CustomerPageComponent },
+  { path: 'delivery-page', component: DeliveriesPageComponent },
+  { path: 'order-page', component: OrderPageComponent },
+  { path: 'product-page', component: ProductPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
